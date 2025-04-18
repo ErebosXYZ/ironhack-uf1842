bf6d432c7cd8d9bd6f828d257d0e1cf2b90aa12b
ErebosXYZ <robertmaynou@gmail.com>
2025-04-18 18:49:43 +0200

    done

--- dom/app.js
+++ dom/app.js
@@ -1,1 +1,29 @@
-// ¿En tu páis de origen es normal dejar propina?
+// ¿En tu páis de origen es normal dejar propina? No porque somos muy agarrados.
+
+const billAmount = document.querySelector("#bill-amount");
+
+const percentageTip = document.querySelector("#percentage-tip");
+
+const tipAmount = document.querySelector("#tip-amount");
+
+const total = document.querySelector("#total");
+
+const calculate = document.querySelector("#calculate");
+
+calculate.addEventListener("click", () => {
+    const billValue = Number(billAmount.value);
+    const percentage = Number(percentageTip.value / 100);
+
+    // Hay que convertir los valores iniciales a número porque el input por defecto es un string
+
+    console.log(billValue, percentage);
+    
+    tip = billValue * percentage;
+    console.log(tip);
+
+    tipAmount.value = tip.toFixed(2);
+
+
+    total.value = (billValue + tip).toFixed(2);
+    console.log(total.value);
+})
