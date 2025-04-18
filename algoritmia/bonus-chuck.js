8059b747e70db6e49a5691eacd9dd88367b443ed
ErebosXYZ <robertmaynou@gmail.com>
2025-04-18 19:33:29 +0200

    done

--- algoritmia/bonus-chuck.js
+++ algoritmia/bonus-chuck.js
@@ -1,7 +1,9 @@
 /** Este ejercicio no puntúa. Podéis hacerlo bonus- https://www.codewars.com/kata/570669d8cb7293a2d1001473 . La función siempre devolver false, pero NO podéis utilizar la palabra false */
 
 function ifChuckSaysSo() {
-
+    const value1 = 1;
+    const value0 = 0;
+    return value1 === value0;
 }
 
-console.log(ifChuckSaysSo()); // false
+console.log(ifChuckSaysSo(true)); // false
